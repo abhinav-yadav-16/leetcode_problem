71573672c5718f4294b1295bf11b7e5e76108244
abhinav-yadav-16 <133758961+abhinav-yadav-16@users.noreply.github.com>
2024-09-03 22:51:01 +0530

    Time: 9 ms (32.72%), Space: 42.5 MB (44.97%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,8 @@
+import java.math.BigInteger;
+class Solution {
+    public String multiply(String num1, String num2) {
+        BigInteger a  = new BigInteger(num1), b  = new BigInteger(num2);
+        BigInteger c  = a.multiply(b);
+        return c.toString();
+    }
+}
